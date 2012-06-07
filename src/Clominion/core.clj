6898999ec79cce93246e3fae498c92e551ccb7b9
Dennis Laumen <dennis@dennislaumen.nl>
2012-06-07 22:05:21 +0200

    Made starting game fact be confirmed for two players and fail for two.

--- src/Clominion/core.clj
+++ src/Clominion/core.clj
@@ -3,4 +3,4 @@
 (defn start-game
     [num-players]
     {:pre [(> num-players 1) (< num-players 5)]}
-    [:a :b :c :d])
+    (repeat num-players :a))
--- test/Clominion/test/core.clj
+++ test/Clominion/test/core.clj
@@ -4,6 +4,7 @@
 
 (fact "Clominion can be played with two to four players"
     (start-game 1) => (throws AssertionError)
+    (start-game 2) => (two-of keyword?)
     (start-game 4) => (four-of keyword?)
     (start-game 5) => (throws AssertionError)
 )
